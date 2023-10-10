06ef2b2ec7d0d65eeafc1f3a5e8ee579aac25c06
GOODNESSMAY <goodnespeace651@gmail.com>
2023-10-10 16:19:14 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 import sys
 
-number = len(sys.argv) - 1
-if number == 0:
-    print("0 arg.")
-elif number == 1:
-    print("1 arg:")
-else:
-    print("{} arg:".format(number))
-for m in range(number):
-    print("{}: {}".format(m + 1, sys.argv[m + 1]))
+def argument_returned():
+  number = len(sys.argv) - 1
+  if number == 0:
+    print("No arguments were passed.")
+  elif number == 1:
+    print("1 argument was passed:")
+    print(sys.argv[1])
+  else:
+    print(f"{argument_returned} arguments were passed:")
+  for arg in sys.argv[1:]:
+    print(arg)
+
+
+argument_returned()
