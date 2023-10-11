2501e4076149106a706d435b5b282c0b91774c58
GOODNESSMAY <goodnespeace651@gmail.com>
2023-10-11 03:12:04 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-import sys
 
-number = len(sys.argv) - 1
-if number == 0:
-    print("0 arguments.")
-elif number == 1:
-    print("1 argument:")
-else:
-    print("{} arguments:".format(number))
-for m in range(number):
-    print("{}: {}".format(m + 1, sys.argv[m + 1]))
+if__name__ == "__main__":
+
+    import sys
+
+    number = len(sys.argv) - 1
+    if number == 0:
+        print("0 arguments.")
+    elif number == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(number))
+    for m in range(number):
+        print("{}: {}".format(m + 1, sys.argv[m + 1]))
 
