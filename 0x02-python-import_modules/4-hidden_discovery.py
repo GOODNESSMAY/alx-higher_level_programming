03f6421bcea3ddc258114f10fdfd903a828d47c7
GOODNESSMAY <goodnespeace651@gmail.com>
2023-10-11 03:35:38 -0700

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     import hidden_4
     for name in dir(hidden_4):
         if name[:2] != "__":
-            print("{:name}".format(name))
+            print("{name}".format(name))
