907504d043ccdab3f4a05aca6543abdc10963846
GOODNESSMAY <goodnespeace651@gmail.com>
2023-11-08 05:55:33 -0800

    all tasks

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""
+function that append a string
+"""
+
+def append_write(filename="", text=""):
+    """return number of characters added:"""
+    with open(filename, 'a', encoding='utf=8') as f:
+        return f.write(text)
