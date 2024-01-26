a4072e2a054df0d30ab9931fbcabc43e65fa945b
GOODNESSMAY <goodnespeace651@gmail.com>
2024-01-25 16:58:50 -0800

    test-run

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a peak-finding algorithm."""
+
+
+def find_peak(list_of_integers):
+    """ Finds the peak in a list of integers """
+    if list_of_integers == []:
+        return None
+
+
